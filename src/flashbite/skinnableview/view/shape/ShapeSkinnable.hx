9738d9279c5047be03dbcfd9b1c1ed122e0ffeea
barbudumitruadrian <barbudumitruadrian@gmail.com>
2018-10-05 12:40:29 +0300

    ShapeSkinnable - now is using a Bitmap in order be visible when a BitmapData.draw is performed on it.

--- src/flashbite/skinnableview/view/shape/ShapeSkinnable.hx
+++ src/flashbite/skinnableview/view/shape/ShapeSkinnable.hx
@@ -3,7 +3,9 @@ package flashbite.skinnableview.view.shape;
 import flashbite.skinnableview.model.ISkinnableData;
 import flashbite.skinnableview.model.skinstyle.ISkinObject;
 import flashbite.skinnableview.view.ISkinnableView;
-import openfl.display.Shape;
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
+import openfl.display.PixelSnapping;
 
 /**
  * QuadSkinnable class; Extends openfl.display.Shape and draw the skin rectangle
@@ -11,7 +13,7 @@ import openfl.display.Shape;
  * @author Adrian Barbu
  */
 @:final
-class ShapeSkinnable extends Shape implements ISkinnableView
+class ShapeSkinnable extends Bitmap implements ISkinnableView
 {
 	public var skinObj(get, null):ISkinObject;
 	
@@ -23,10 +25,12 @@ class ShapeSkinnable extends Shape implements ISkinnableView
 	
 	public function new(skinObj:ISkinObject, skinnableData:ISkinnableData)
 	{
-		super();
-		
 		this.skinObj = skinObj;
+		
+		super(new BitmapData(1, 1, false, 0xFFFFFF), PixelSnapping.NEVER);
+		
 		color = skinObj.color;
+		
 		redraw();
 	}
 	
@@ -43,6 +47,7 @@ class ShapeSkinnable extends Shape implements ISkinnableView
 	public function dispose():Void
 	{
 		skinObj = null;
+		this.bitmapData.dispose();
 	}
 	
 	// ====================================================================================================================================
@@ -53,21 +58,8 @@ class ShapeSkinnable extends Shape implements ISkinnableView
 	{
 		this.x = skinObj.x;
 		this.y = skinObj.y;
-		
-		//draw
-		draw();
-	}
-	
-	// ====================================================================================================================================
-	// PRIVATE
-	// ====================================================================================================================================
-	
-	private function draw():Void
-	{
-		this.graphics.clear();
-		this.graphics.beginFill(Std.int(Math.max(0, color)), skinObj.alpha);
-		this.graphics.drawRect(0, 0, skinObj.width, skinObj.height);
-		this.graphics.endFill();
+		this.width = skinObj.width;
+		this.height = skinObj.height;
 	}
 	
 	// ====================================================================================================================================
@@ -80,8 +72,11 @@ class ShapeSkinnable extends Shape implements ISkinnableView
 	function set_color(value:Int):Int
 	{
 		if (color != value && value >= 0) {
+			var colorInfo = this.transform.colorTransform;
+			colorInfo.color = value;
+			this.transform.colorTransform = colorInfo;
+			
 			color = value;
-			draw();
 		}
 		
 		return color;
